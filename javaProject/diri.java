73ae001ecb7d617384d5e2de2c55548f03ec5f5e
aleaf <96neko_._tanker@gmail.com>
2025-07-24 18:51:05 +0700

    testing

---
+++ javaProject/diri.java
@@ -0,0 +1,19 @@
+public class diri {
+
+    String name = "alif";
+    String kelas = "11 rpl";
+    String alamat = "kairacondong";
+    String nis = "123456789";
+    String phone = "08888888888";
+    String email = "alif@example.com";
+
+    public static void main(String[] args) {
+        diri data = new diri();
+        System.out.println(data.name);
+        System.out.println(data.kelas);
+        System.out.println(data.alamat);
+        System.out.println(data.nis);
+        System.out.println(data.phone);
+        System.out.println(data.email);
+    }
+}
