dfa560a05c605aa895d1b01ddbfa8a4e1030b367
aleaf <96neko_._tanker@gmail.com>
2025-07-25 21:20:03 +0700

    testing git

--- javaProject/test.java
+++ javaProject/test.java
@@ -1,5 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("whats wrong with this code?");
+    }
+}
+
+public class test2 {
+    public static void main(String[] args) {
+        System.out.println("nothing is wrong with this code");
     }
 }
